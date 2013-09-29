be1ddb3771c8a56aed5d10aa86c6f3a3939e0cbc
Cinghio <cinghio@linuxmail.org>
2013-09-29 15:09:38 +0200

    aspettando gli ultimi ordini

--- castagno.py
+++ castagno.py
@@ -1,7 +1,7 @@
 
 class Pasta():
     def __init__(self,s):
-        self.cod = s[0]
+        self.cod = s[0].strip('"')
         self.cod_ean = s[1]
         self.cod_intra = s[2]
         self.descrizione = s[3]#[:40]
@@ -21,16 +21,23 @@ class Ordine():
         self.cod = cod
         self.num = numero
     def totale(self,P):
-        return P[self.cod].prezzo * P[self.cod].pz * (1+P[self.cod].iva)
+        return prezzo (self.cod,P)
+        #return P[self.cod].prezzo * P[self.cod].pz * (1+P[self.cod].iva)
     def __str__(self):
         return self.cod+' - {}'.format(self.num)
         
+def prezzo (cod,P,num=None):
+    prezzo_pacco = P[cod].prezzo * P[cod].pz * (1+P[cod].iva)
+    if num is not None:
+        prezzo_pacco *= num
+    return prezzo_pacco
+
 def read_file(filename):
     P={}
     with open(filename,'r') as fin:
         for line in fin:
             s = line.split(',')
-            P[s[0]]=Pasta(s)
+            P[s[0].strip('"')]=Pasta(s)
     return P
 
 def leggi_ordini(filename):
@@ -64,16 +71,27 @@ def main():
 
     ordi = leggi_ordini('ordini.ini')
     totali = {}
+    paste = {}
     for gente in ordi:
         print ('---',gente)
         tot = 0
         for pasta in ordi[gente]:
             par_tot = pasta.totale(P)
             print ('{0:12s} - {1:12.2f}'.format(pasta, par_tot))
-            #print ('-------------------',P[pasta.cod].descrizione)
             tot += par_tot
+            if pasta not in paste:
+                paste[pasta.cod] = pasta.num
+            else:
+                paste[pasta.cod] += pasta.num
         totali[gente] = tot
-        print ('totale = {0:12.2f}'.format(tot))
+        print ('totale {0:25s}= {1:8.2f}'.format(gente,tot))
+
+    print ('\nOrdine Totale:')
+    for pasta in paste.keys():
+        prz = prezzo(pasta,P,num=paste[pasta])
+        print ('{0:12s} num {1:3d} prezzo {2:6.2f}'.format(pasta,\
+                                                         paste[pasta],\
+                                                         prz))
     print ('\nSpesa Totale = {0:12.2f}'.format(sum(totali.values())))
     return 0
 
